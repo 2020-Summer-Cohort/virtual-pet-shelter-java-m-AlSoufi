67b5e7abc63da408f7937a4141e6906db1d5be98
m-alsoufi <mo7ammad.alsoufi@gmail.com>
2020-05-21 19:17:30 -0400

    starting repo

---
+++ src/main/java/shelter/VirtualPet.java
@@ -0,0 +1,73 @@
+package shelter;
+
+public class VirtualPet {
+    private String name;
+    private String desc;
+    private int hunger;
+    private int thirst;
+    private int boredom;
+
+    public VirtualPet(String name, String desc) {
+        this.name = name;
+        this.desc = desc;
+    }
+
+    public VirtualPet(String name, String desc, int hunger, int thirst, int boredom) {
+        this.name = name;
+        this.desc = desc;
+        this.hunger = hunger;
+        this.thirst = thirst;
+        this.boredom = boredom;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public int getHunger() {
+        return hunger;
+    }
+
+    public int getThirst() {
+        return thirst;
+    }
+
+    public int getBoredom() {
+        return boredom;
+    }
+
+    public void feeding() {
+        System.out.println(hunger -= 35);
+
+    }
+
+    public void watering() {
+        System.out.println(thirst -= 35);
+
+    }
+
+    public void playing() {
+        System.out.println(boredom -= 15);
+
+    }
+
+    public void takeToDoctor() {
+        System.out.println(sickness -= 25);
+
+    }
+
+    public void tick() {
+        hunger += 5;
+        thirst += 5;
+        boredom += 5;
+    }
+
+
+}
+
+
+
--- src/main/java/shelter/VirtualPetShelterApp.java
+++ src/main/java/shelter/VirtualPetShelterApp.java
@@ -1,4 +1,28 @@
 package shelter;
 
 public class VirtualPetShelterApp {
+    public static void main(String[] args) {
+
+        VirtualPet pet1 = new VirtualPet("Joey", "Frisky", 20, 20, 20);
+        VirtualPet pet2 = new VirtualPet("Jordy", "Fuzzy", 20, 20, 20);
+        VirtualPet pet3 = new VirtualPet("Jerry", "Orange", 20, 20, 20);
+        VirtualPet pet4 = new VirtualPet("Bubbles", "Lazy", 20, 20, 20);
+
+        System.out.println("Thank you for volunteering at The Neighborhood Virtual Pet Shelter!");
+        System.out.println("This is the status of our pets currently:");
+        System.out.println("Name	|   Hunger	|   Thirst	|  Boredom");
+        System.out.println("--------|-----------|-----------|-------");
+        System.out.println(pet1.getName() + "    | " + pet1.getHunger() + "        | " + pet1.getThirst() + "        | " + pet1.getBoredom());
+        System.out.println(pet2.getName() + "   | " + pet2.getHunger() + "        | " + pet2.getThirst() + "        | " + pet2.getBoredom());
+        System.out.println(pet3.getName() + "   | " + pet3.getHunger() + "        | " + pet3.getThirst() + "        | " + pet3.getBoredom());
+        System.out.println(pet4.getName() + " | " + pet4.getHunger() + "        | " + pet4.getThirst() + "        | " + pet4.getBoredom());
+        System.out.println("What would you like to do next?");
+        System.out.println("1. Feed the pets");
+        System.out.println("2. Water the pets");
+        System.out.println("3. Play with a pet");
+        System.out.println("4. Adopt a pet");
+        System.out.println("5. Admit a pet");
+        System.out.println("6. Quit");
+
+    }
 }
